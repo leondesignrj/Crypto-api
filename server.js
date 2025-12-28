4cda723b1e7f9dea4635b6bf4c3a9ca93c376183
leondesignrj <leondesignrj@gmail.com>
2025-12-28 01:35:13 -0300

    Update server.js

--- server.js
+++ server.js
@@ -5,8 +5,6 @@ app.get('/', (req, res) => {
   res.json({ status: 'API crypto funcionando' });
 });
 
-const PORT = process.env.PORT || 3000;
-
-app.listen(PORT, () => {
-  console.log(`Servidor activo en puerto ${PORT}`);
+app.listen(3000, () => {
+  console.log('Servidor activo en puerto 3000');
 });
